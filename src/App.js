4664c15f76ced07aaf06f95c98d8b8dfbf53702e
jdephil <jennifer.dephillips@gmail.com>
2020-08-12 18:16:07 -0400

    started on circle

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Link, Redirect} from "react-router-dom";
 import Home from './Home';
+import ActivityTracker from './components/ActivityTracker'
+
 
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
     <div className="App">
       our hackathon project!
       <Route exact path='/home' component={Home} />
+      <Route path='/activityTracker' component={ActivityTracker} />
     </div>
   );
 }
