4c5d4819c21450d5708bc70148f3fe0836b08a77
karlmiche <karlymism@gmail.com>
2020-08-12 20:33:16 -0700

    realigned divs

--- src/components/PictureOptions.js
+++ src/components/PictureOptions.js
@@ -2,13 +2,17 @@ import React from 'react';
 import FormGroup from '@material-ui/core/FormGroup';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import './PictureOptions.css';
+import Grid from '@material-ui/core/Grid';
 
 const PictureOptions = () => {
 
     return (
         <div className="options-container">
-            <div className="options-row">
-                <div>
+            <Grid container>
+                <Grid item xs={6}>
+                <div className="options-row">
+                <div className="bandage">
                     <img className="option" src='./bandage.png' />
                 <FormControlLabel
                     control={<Checkbox 
@@ -16,21 +20,23 @@ const PictureOptions = () => {
                     label="Secondary"
                 />
                 </div>
-                <div>
+                <div className="bone">
                     <img className="option" src='./bone_1.png' />
                 <FormControlLabel
                     control={<Checkbox 
                     name="checkedA" />}
                     label="Secondary"
                 />
-                </div><div>
+                </div>
+                <div className="drug">
                     <img className="option" src='./drug_3.png' />
                 <FormControlLabel
                     control={<Checkbox 
                     name="checkedA" />}
                     label="Secondary"
                 />
-                </div><div>
+                </div>
+                <div className="drug-two">
                     <img className="option" src='./drug_5.png' />
                 <FormControlLabel
                     control={<Checkbox 
@@ -39,7 +45,8 @@ const PictureOptions = () => {
                 />
                 </div>
             </div>
-            <div className="options-row">
+                </Grid>
+                <Grid item xs={6}>
                 <div>
                     <img className="option" src='./stethoscope.png' />
                 <FormControlLabel
@@ -69,7 +76,8 @@ const PictureOptions = () => {
                     label="Secondary"
                 />
                 </div>
-            </div>
+                </Grid>
+            </Grid>
         </div>
     );
 };
